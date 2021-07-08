59f2d88de1b5d07a4d46e5ddf1f69f54d5e6766a
khong.hung <ahihimaianh3@gamil.com>
2021-07-09 02:18:53 +0700

    Bai-Tap

---
+++ su-dung-toan-tu.js
@@ -0,0 +1,12 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("chieu dai");
+inputHeight = prompt("chieu rong");
+
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let area = width * height;
+document.write("dien tich: " + area );
